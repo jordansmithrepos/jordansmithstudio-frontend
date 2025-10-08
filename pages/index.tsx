3a77d88b14f54d22d6aa598e3ad543fcb962a45e
jsmith-bhi <55511566+jsmith-bhi@users.noreply.github.com>
2025-10-08 12:18:01 -0500

    Init

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import Head from "next/head";
+// import Image from "next/image";
+// import { Courier_Prime } from "next/font/google";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Jordan Smith Studio</title>
+        <meta name="description" content="The web home of Jordan Smith's artistic endeavors." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/jss_multiplied_favicon.png" />
+      </Head>
+      <main>
+        <h1>Jordan Smith Studio</h1>
+      </main>
+      <footer>
+      </footer>
+    </>
+  );
+}
